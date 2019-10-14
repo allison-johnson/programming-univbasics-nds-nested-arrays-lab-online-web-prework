036f95664fa04a9ac31d41112c26431dc9342b72
Allison Johnson <allison.cuttler@gmail.com>
2019-10-14 16:41:27 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,18 +17,19 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  # Build an array that contains (or, "nests") the residents of The Ennet House
-  # and the Enfield Tennis Academy as provided by the constants
+  array = [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  array
 end
 
 def array_literal_matrix
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
-  # alphabetically by the first character.
+  ENNET_HOUSE_NEW = ENNET_HOUSE.sort
+  ENFIELD_TENNIS_ACADEMY_NEW = ENFIELD_TENNIS_ACADEMY.sort
+  array = [ENNET_HOUSE_NEW, ENFIELD_TENNIS_ACADEMY_NEW]
+  array
 end
 
 def matrix_lookup(matrix, row, column)
-  # Return the matrix's content at row and column
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
